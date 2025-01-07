e116042c67bcdc217fa643b234e6f7c00bea7b22
Icaro Molinari <icaro@coprodux.com>
2025-01-07 00:43:07 -0300

    Express rodando e funcional.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
 import express from "express";
+
+const PORT = 3333;
+
+const app = express();
+
+app.get("/company/:id", (request, response) => {
+  const { id } = request.params;
+  response.send(`Empresa ${id}`);
+});
+
+app.listen(PORT, () =>
+  console.log(
+    `Server está sendo executado na porta ${PORT} | Server is already running on port ${PORT}`
+  )
+);
